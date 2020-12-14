c3ffb93b455adb1add90799f95c60f2fbc61e09d
Javier Moreno <mq2880@gmail.com>
2020-12-14 11:40:08 +0100

    Commit

---
+++ IceCreamCompany/gulpfile.js
@@ -0,0 +1,39 @@
+﻿/// <binding ProjectOpened='js-watcher' />
+var gulp = require('gulp');
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var paths = {
+webroot: "./wwwroot/",
+nodeModules: "./node_modules/"
+};
+
+paths.jqueryjs = paths.nodeModules + "jquery/dist/jquery.js";
+paths.destinationjsFolder = paths.webroot + "scripts/";
+paths.vendorjsFileName = "vendor.min.js";
+
+paths.jsFiles = "./Scripts/*.js";
+paths.jsFileName = "script.min.js";
+
+gulp.task("copy-js-file", function () {
+return gulp.src(paths.jqueryjs)
+.pipe(gulp.dest(paths.destinationjsFolder));
+});
+gulp.task("min-vendor:js", function () {
+    return gulp.src(paths.jqueryjs)
+        .pipe(concat(paths.vendorjsFileName))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.destinationjsFolder));
+});
+
+gulp.task("min:js", function () {
+    return gulp.src(paths.jsFiles)
+        .pipe(concat(paths.jsFileName))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.destinationjsFolder));
+});
+
+gulp.task("js-watcher", function () {
+    gulp.watch('./Scripts/*.js', gulp.series("min:js"));
+});
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# 20846C-Mod9-Lab-Exercise1
+# Module 9: Client-Side Development
+
+## Lab: Client-Side Development
+
+1. **Nombres y apellidos:** Francisco Javier Moreno Quevedo
+2. **Fecha:** 02/12/2020
+3. **Resumen del Ejercicio:** Añadir los componentes necesarios para hacer funcional la aplicacion de venta de un Zoo 1/4
+4. **Dificultad o problemas presentados y como se resolvieron:** Ninguna
+
+
+
+- Ejercicio 1: Using gulp to Run Tasks 
+  - Instalamos los paquetes
+  - Creamos un fichero JavaScript **gulpfile.js** con las tareas de copia
+    - Ejecutamos  la tarea
+    - Añadimos una nueva tarea que minimice el fichero Vendor.js
+    - Añadimos una nueva tarea que ninifique los javascript de una carpeta del proyecto
+    - Añadimos un watcher
+    - Ejecutamos las tareas
+
+![](./img/Captura1.jpg)
+
+
+
+Watcher ejecutandose
+
+
+
+![](./img/Captura2.jpg)
